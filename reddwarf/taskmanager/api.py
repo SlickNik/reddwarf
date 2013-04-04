2aa1638a15a7e42c29ffdcfbd9cf80f6470fdd40
Robert Myers <robert.myers@rackspace.com>
2013-04-04 11:22:17 -0500

    Remove duplicate delete method.
    Change-Id: I853cfba8fea2af286dca32b33f3f6c6ad93baeb3

--- reddwarf/taskmanager/api.py
+++ reddwarf/taskmanager/api.py
@@ -99,10 +99,6 @@ class API(ManagerAPI):
         LOG.debug("Making async call to delete backup: %s" % backup_id)
         self._cast("delete_backup", backup_id=backup_id)
 
-    def delete_backup(self, backup_id):
-        LOG.debug("Making async call to delete backup: %s" % backup_id)
-        self._cast("delete_backup", backup_id=backup_id)
-
     def teardown_backup(self, instance_id):
         LOG.debug("Making async call to teardown a backup for instance: %s" %
                   instance_id)
