f62500ea5450dc5d1ccbef01e9f868c7734a42f9
Steve Leon <steve.leon@hp.com>
2013-04-05 17:41:25 -0700

    Adding backup list and delete int tests

--- reddwarf/tests/api/backups.py
+++ reddwarf/tests/api/backups.py
@@ -13,36 +13,31 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 from nose.tools import assert_equal, assert_false, assert_true, assert_raises
-
-from proboscis import before_class
 from proboscis import test
 from reddwarf.tests.fakes.swift import SwiftClientStub
 from reddwarfclient import exceptions
 from reddwarf.tests.config import CONFIG
-from reddwarf.tests.util import create_dbaas_client
-from reddwarf.tests.util.users import Requirements
 from reddwarf.tests.api.instances import WaitForGuestInstallationToFinish
 from reddwarf.tests.api.instances import instance_info
 
 
 GROUP = "dbaas.api.backups"
+CREATE_BACKUP_GROUP = "dbaas.api.backups.create"
+LIST_BACKUP_GROUP = "dbaas.api.backups.list"
+DELETE_BACKUP_GROUP = "dbaas.api.backups.delete"
+BACKUP_NAME = 'backup_test'
+BACKUP_DESC = 'test description'
 
 
 @test(depends_on_classes=[WaitForGuestInstallationToFinish],
-      groups=[GROUP])
+      groups=[GROUP, CREATE_BACKUP_GROUP])
 class CreateBackups(object):
-
-    @before_class
-    def setUp(self):
-        reqs = Requirements(is_admin=False)
-        user = CONFIG.users.find_user(reqs)
-        self.rd_client = create_dbaas_client(user)
 
     @test
     def test_backup_create_instance_not_found(self):
         """test create backup with unknown instance"""
 
-        assert_raises(exceptions.NotFound, self.rd_client.backups.create,
+        assert_raises(exceptions.NotFound, instance_info.dbaas.backups.create,
                       'backup_test', 'instance_id', 'test description')
 
     @test
@@ -51,18 +46,57 @@ class CreateBackups(object):
 
         if CONFIG.fake_mode:
             client_stub = SwiftClientStub()
-            client_stub.with_account('tenant_id')
+            client_stub.with_account("tenant_id")
 
-        result = self.rd_client.backups.create('backup_test',
-                                               instance_info.id,
-                                               'test description')
+        result = instance_info.dbaas.backups.create(BACKUP_NAME,
+                                                    instance_info.id,
+                                                    BACKUP_DESC)
 
-        assert_equal('backup_test', result.name)
-        assert_equal('test description', result.description)
+        assert_equal(BACKUP_NAME, result.name)
+        assert_equal(BACKUP_DESC, result.description)
         assert_equal(instance_info.id, result.instanceRef)
         assert_equal('NEW', result.status)
 
-        # Check backup create when another one is running
+
+@test(runs_after_groups=[CREATE_BACKUP_GROUP],
+      groups=[GROUP, LIST_BACKUP_GROUP])
+class ListBackups(object):
+
+    @test
+    def test_backup_create_another_backup_running(self):
+        """test create backup when another backup is running"""
         assert_raises(exceptions.ClientException,
-                      self.rd_client.backups.create,
-                      'backup_test2', instance_info.id, 'test description')
+                      instance_info.dbaas.backups.create,
+                      'backup_test2', instance_info.id, 'test description2')
+
+    @test
+    def test_backup_list(self):
+        """test list backups"""
+
+        result = instance_info.dbaas.backups.list()
+        assert_equal(1, len(result))
+        backup = result[0]
+        assert_equal(BACKUP_NAME, backup.name)
+        assert_equal(BACKUP_DESC, backup.description)
+        assert_equal(instance_info.id, backup.instanceRef)
+        assert_equal('NEW', backup.status)
+
+
+@test(runs_after_groups=[LIST_BACKUP_GROUP],
+      groups=[GROUP, DELETE_BACKUP_GROUP])
+class DeleteBackups(object):
+
+    @test
+    def test_backup_delete_not_found(self):
+        """test delete unknown backup"""
+        assert_raises(exceptions.NotFound, instance_info.dbaas.backups.delete,
+                      'backup_not_existent')
+
+    @test
+    def test_backup_delete_still_running(self):
+        """test delete backup when it is running"""
+        result = instance_info.dbaas.backups.list()
+        backup = result[0]
+        assert_raises(exceptions.ClientException,
+                      instance_info.dbaas.backups.delete,
+                      backup.id)
